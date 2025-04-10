2071e94990874d0871b961ed5406d4414103b719
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-10 09:39:23 +0100

    refactor: rename tests

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -1,13 +1,13 @@
 import { stringCalculatorV1 } from '../core/stringCalculator';
 
 describe('String Calculator version 1 should', () => {
-  it('return 0 when the input is an empty text', () => {
+  it('return 0 for an empty text', () => {
     expect(stringCalculatorV1('')).toBe(0);
   });
-  it('return 0 when the input is null', () => {
+  it('return 0 for null', () => {
     expect(stringCalculatorV1(null)).toBe(0);
   });
-  it('return a number when the input is a number in format text', () => {
+  it('return the same number for a number in format text', () => {
     expect(stringCalculatorV1('56')).toBe(56);
   });
 });
