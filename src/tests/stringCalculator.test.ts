ce7ac070ebc9d6717a998a208a72393bc08aa707
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-10 09:23:56 +0100

    test: add new test case for null input

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -1,7 +1,10 @@
 import { stringCalculatorV1 } from '../core/stringCalculator';
 
-describe("String Calculator version 1 should", () => {
-  it("return 0 when the input is an empty text", () => {
-    expect(stringCalculatorV1("")).toBe(0);
-  })
-})
+describe('String Calculator version 1 should', () => {
+  it('return 0 when the input is an empty text', () => {
+    expect(stringCalculatorV1('')).toBe(0);
+  });
+  it('return 0 when the input is null', () => {
+    expect(stringCalculatorV1(null)).toBe(0);
+  });
+});
